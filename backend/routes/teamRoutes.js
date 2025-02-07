9b462ae791548799e0c09ada7b8ac297dc81adbe
RhythmForge1 <gv2062549@gmail.com>
2025-02-07 15:22:25 +0530

    new versions

--- backend/routes/teamRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const teamController = require('../Controllers/TeamController');
-
-// Routes
-router.post('/add-team', teamController.addTeam); // Add a new team
-router.get('/get-teams', teamController.getTeams); // Fetch all teams
-
-module.exports = router;
